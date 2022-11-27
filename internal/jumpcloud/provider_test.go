5ed7cf034f8fe5a5d005c0ea905cbf08793fe3cf
chrisisbeef <chrisisbeef@gmail.com>
2022-11-27 11:42:56 -0700

    added variable for api key for acc tests

--- internal/jumpcloud/provider_test.go
+++ internal/jumpcloud/provider_test.go
@@ -1,20 +1,21 @@
 package jumpcloud
 
 import (
-    "github.com/hashicorp/terraform-plugin-framework/providerserver"
-    "github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 )
 
 const (
 	providerConfig = `
+variable "jumpcloud_api_key" {}
 provider "jumpcloud" {
-	api_key = "test"
+	api_key = "${ var.jumpcloud_api_key }"
 }
 `
 )
 
 var (
-	testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error) {
+	testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
 		"jumpcloud": providerserver.NewProtocol6WithError(New("dev")()),
 	}
 )
