dcd70c108c688c3cded6e5ed243bf2c8d33b80ff
chrisisbeef <chrisisbeef@gmail.com>
2022-11-27 20:14:06 -0700

    found the ExpectError attribute, neat - so let's give it a shot

--- internal/jumpcloud/ad_resource_test.go
+++ internal/jumpcloud/ad_resource_test.go
@@ -1,6 +1,7 @@
 package jumpcloud
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -32,10 +33,8 @@ func TestAccActiveDirectoryResource(t *testing.T) {
 			},
 			// Update is not supported -- how do we test for expecting an error tho?
 			{
-				Config: ProviderConfig() + `resource "jumpcloud_ad" "test" { domain = "DC=update,DC=test,DC=com" }`,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("jumpcloud_ad.test", "domain", "DC=update,DC=test,DC=com"),
-				),
+				Config:      ProviderConfig() + `resource "jumpcloud_ad" "test" { domain = "DC=update,DC=test,DC=com" }`,
+				ExpectError: regexp.MustCompile(".*"),
 			},
 			// Delete Testing happens automatically
 		},
