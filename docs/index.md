99448e910db4fd27699bdf6511abe1fc19d461b7
chrisisbeef <chrisisbeef@gmail.com>
2022-11-29 17:28:20 -0700

    fixed generate

--- docs/index.md
+++ docs/index.md
@@ -13,6 +13,14 @@ description: |-
 ## Example Usage
 
 ```terraform
+terraform {
+  required_providers {
+    jumpcloud = {
+      source = "techjavelin/jumpcloud"
+    }
+  }
+}
+
 provider "jumpcloud" {
   api_key = "API_KEY_GOES_HERE"
 }
--- examples/resources/jumpcloud_usergroup/resource.tf
+++ examples/resources/jumpcloud_usergroup/resource.tf
@@ -1,4 +1,45 @@
 resource "jumpcloud_usergroup" "example" {
-  name = "example"
-  
+  name        = "example"
+  description = "Example Description"
+  email       = "example@example.com"
+
+  sudo = {
+    enabled           = false
+    passwpasswordless = false
+  }
+
+  ldap = {
+    groups = [
+      { name = "example" }
+    ]
+  }
+
+  posix = [
+    {
+      id   = 1000
+      name = "example"
+    }
+  ]
+
+  radius = [
+    {
+      name  = "example"
+      value = "example"
+    }
+  ]
+
+  samba = false
+
+  member_queries = [
+    {
+      query = {
+        field    = "example"
+        operator = "eq"
+        value    = "example"
+      }
+    }
+  ]
+
+  notify = false
+  auto   = false
 }
